b15e8e7e166d2d1fc5539c4bc9f345b54f9142b8
obayalhusein <obayalhusein@gmail.com>
2022-10-25 19:30:52 +0300

    add input field to the homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,7 +6,11 @@
           <v-col>
             <h1 class="display-4">Start selling <br><span class="deep-purple--text text--accent-3">online</span> now!</h1>
             <p>Build your own e-commerce website and mobile app in few minutes</p>
-            <v-btn to="/setup/store-category"  color="deep-purple accent-3" class="white--text">Get Started</v-btn>
+            <div class="d-flex align-items-center">
+              <v-text-field label="Your email or phone" hide-details outlined>
+              </v-text-field>
+              <v-btn to="/setup/store-category" color="deep-purple accent-3" class="white--text my-0" x-large>Get Started</v-btn>
+            </div>
           </v-col>
           <v-col></v-col>
         </v-row>
