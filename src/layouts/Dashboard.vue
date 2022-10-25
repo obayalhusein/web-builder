9bee18fa78c235709f23cff5f8f7a12a868d88c8
obayalhusein <obayalhusein@gmail.com>
2022-10-25 18:55:26 +0300

    change dashboard icons

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -7,15 +7,49 @@
               </g-link>
           </v-toolbar-title>
 
-          <v-btn dark text>Platform</v-btn>
+          <v-menu left bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn v-bind="attrs" v-on="on" dark text>
+                Platform
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>Website</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Mobile App</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>POS</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          
           <v-btn dark text>Products</v-btn>
           <v-btn dark text>Orders</v-btn>
 
           <v-spacer></v-spacer>
           
-          <v-btn dark text>Notifcations</v-btn>
-          <v-btn dark text>Settings</v-btn>
-          <v-btn dark text class="me-16">Help</v-btn>
+          <v-btn dark text icon><i class="fa-solid fa-bell"></i></v-btn>
+          <v-btn dark text icon><i class="fa-solid fa-gear"></i></v-btn>
+          <v-btn dark text icon><i class="fa-solid fa-circle-info"></i></v-btn>
+
+          <v-menu left bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn v-bind="attrs" v-on="on" dark text class="me-16">email@domain.com &nbsp;&nbsp;<i class="fa-solid fa-caret-down"></i></v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>Profile</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Logout</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
 
           <template v-slot:extension class="px-0">
             <v-container class="white px-16" fluid>
@@ -23,12 +57,15 @@
                 <div style="width: 200px">
                   <v-select :items="items" label="Page" class="ms-10" hide-details dense outlined />
                 </div>
-                <v-btn text>M</v-btn>
-                <v-btn text>D</v-btn>
+                <v-btn text><i class="fa-solid fa-display"></i></v-btn>
+                <v-btn text><i class="fa-solid fa-mobile-screen-button"></i></v-btn>
 
                 <v-spacer></v-spacer>
 
-                <v-btn to="/website" class="text-decoration-underline" text>batata.com</v-btn>
+                <v-btn to="/website" target="_blank" text>
+                  <i class="fa-solid fa-arrow-up-right-from-square"></i>&nbsp;
+                  <span class="text-decoration-underline">batata.com</span>
+                </v-btn>
                 <v-btn to="/" color="grey darken-4" dark>Publish</v-btn>
               </div>
             </v-container>
@@ -46,7 +83,7 @@ export default {
   name: 'DashboardLayout',
   data() {
     return {
-      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+      items: ['Homepage', 'About', 'Contact', 'Products'],
     }
   },
 }
